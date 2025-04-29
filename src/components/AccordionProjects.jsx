17e9f815802ada07e2bbdee01bfefc312d159015
selton.uy <selton.bae@gmail.com>
2025-04-29 12:19:36 -0300

    corrigindo erro

--- src/components/AccordionProjects.jsx
+++ src/components/AccordionProjects.jsx
@@ -6,10 +6,8 @@ import AccordionDetails, {
 } from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import Fade from "@mui/material/Fade";
 import { Grow } from "@mui/material";
 
-<<<<<<< HEAD
 export default function AccordionTransition({
   projectName,
   description,
@@ -17,7 +15,7 @@ export default function AccordionTransition({
   link,
   isExpanded,
   handleChange,
-}) {
+}) {  // <-- Faltava esta chave de abertura
   return (
     <Accordion
       expanded={isExpanded}
@@ -66,57 +64,5 @@ export default function AccordionTransition({
         </div>
       </AccordionDetails>
     </Accordion>
-=======
-export default function AccordionTransition({projectName,Description, Img}) {
-  const [expanded, setExpanded] = React.useState(false);
-
-  const handleExpansion = () => {
-    setExpanded((prevExpanded) => !prevExpanded);
-  };
-
-  return (
-    <div>
-      <Accordion
-        expanded={expanded}
-        onChange={handleExpansion}
-        slots={{ transition: Fade }}
-        slotProps={{ transition: { timeout: 500 } }}
-        sx={{
-          width: "50vw",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          backgroundColor: expanded ? "#169976" : "#222", // muda a cor
-          boxShadow: expanded ? "0 4px 8px rgba(0,0,0,0.1)" : "none",
-          borderRadius: 2,
-          marginBottom: 1,
-          color:'#f2f2f2',
-          [`& .${accordionClasses.region}`]: {
-            height: expanded ? "auto" : 0,
-          },
-          [`& .${accordionDetailsClasses.root}`]: {
-            display: expanded ? "block" : "none",
-          },
-        }}
-      >
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1-content"
-          id="panel1-header"
-        >
-          <Typography component="span">{projectName}</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <div className="flex flex-row gap-20">
-            <img src={Img} alt="imagem do projeto" />
-          <Typography>
-            {Description}a
-          </Typography>
-          </div>
-          
-        </AccordionDetails>
-      </Accordion>
-    </div>
->>>>>>> 99d5802ead4c4db7ef54b8416c6991f5b09f5394
   );
 }
